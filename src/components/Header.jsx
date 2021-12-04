70cf37f72792f655fe0b3acc273052d55ee40300
QSche <markboga@mail.ru>
2021-12-04 16:41:07 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+      return (
+            <header className='header'>
+                  <img   src="https://img.championat.com/s/735x490/news/big/x/e/v-znachit-virtus-pro-organizacija-predstavila-novyj-logotip_15422783561214372278.jpg" />
+            </header>
+      )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Profile = () => {
+      return (
+            <div className='content'>
+                  <div>
+                        <img src="https://s1.1zoom.ru/b5050/630/DOTA_2_Templar_Assassin_501858_2560x1440.jpg"/>
+                  </div>
+                   <div>
+                        ava + description
+                  </div>
+                  <div>
+                        Posts
+                        <div>
+                               New post
+                        </div>
+                        <div>
+                              <div>
+                                    post 1
+                              </div>
+                              <div>
+                                     post 2
+                              </div>
+                        </div>
+                   </div>
+            </div>
+      )
+}
+
+export default Profile
